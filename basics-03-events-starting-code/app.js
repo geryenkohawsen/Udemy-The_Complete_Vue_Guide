fe69750cdb3907c0658547a33b31c29db82f9205
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-09 10:05:27 +0900

    event modifiers

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,22 +1,30 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: '',
-    };
-  },
-  methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
-    },
-    add(num) {
-      this.counter += num;
-      // this.counter = this.counter + 1;
-    },
-    reduce(num) {
-      this.counter -= num;
-    },
-  },
+	data() {
+		return {
+			counter: 0,
+			name: '',
+			confirmedName: '',
+		};
+	},
+	methods: {
+		confirmInput() {
+			this.confirmedName = this.name;
+		},
+		submitForm() {
+			// event.preventDefault(); //tell the browser we dont want the browser default (form submission)
+			alert('Submitted!');
+		},
+		setName(event, lastName) {
+			this.name = event.target.value + ' ' + lastName;
+		},
+		add(num) {
+			this.counter += num;
+			// this.counter = this.counter + 1;
+		},
+		reduce(num) {
+			this.counter -= num;
+		},
+	},
 });
 
 app.mount('#events');
