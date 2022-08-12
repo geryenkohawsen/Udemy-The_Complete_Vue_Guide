33f23f04b1844d2472e4be3d4aaba69a87d2beef
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-12 17:46:59 +0900

    rendering lists of data

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -1,8 +1,15 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			enteredGoalValue: '',
 			goals: [],
 		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredGoalValue);
+			this.enteredGoalValue = '';
+		},
 	},
 });
 
