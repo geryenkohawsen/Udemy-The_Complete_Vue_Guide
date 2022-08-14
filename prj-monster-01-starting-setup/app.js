13ab3eda814a51f7b74d7df697818ed292003f3a
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-14 19:44:43 +0900

    finish battle log and update health bar styling

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -9,13 +9,20 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -43,11 +50,19 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       //   console.log('Player DMG: ' + attackValue);
+      this.addLogMessage('player', 'attack', attackValue);
       this.attackPlayer();
     },
     attackPlayer() {
@@ -56,12 +71,15 @@ const app = Vue.createApp({
       //   console.log('Monster DMG: ' + attackValue);
       //   console.log('Player HP:' + this.playerHealth);
       //   console.log('Monster HP:' + this.monsterHealth);
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       //   console.log('Player DMG: ' + attackValue);
+      this.addLogMessage('player', 'special-attack', attackValue);
+
       this.attackPlayer();
     },
     healPlayer() {
@@ -73,7 +91,18 @@ const app = Vue.createApp({
         this.playerHealth += healValue;
       }
       //   console.log('heal for: ' + healValue);
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
+    },
+    surrender() {
+      this.winner = 'monster';
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
     },
   },
 });
