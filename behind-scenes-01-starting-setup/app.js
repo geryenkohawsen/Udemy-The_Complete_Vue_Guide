636f02a37608e9d3312820b2528feb93a1bee514
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-14 23:01:57 +0900

    test reactive framework with plain JS

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -16,3 +16,24 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// simple reactive framework
+const data = {
+  message: 'Hello!',
+  longMessage: 'Hello! World!',
+};
+
+const handler = {
+  set(target, key, value) {
+    if (key === 'message') {
+      target.longMessage = value + 'World!';
+    }
+    target.message = value;
+  },
+};
+
+const proxy = new Proxy(data, handler);
+
+proxy.message = 'Hi there!';
+
+console.log(proxy.longMessage);
