0a9ecec1ebda6c93f4b120de57b3e488f9d4817d
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-16 19:50:52 +0900

    create basic vue app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+	<section>
+		<h2>My Friends</h2>
+		<ul>
+			<li></li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			friends: [
+				{
+					id: 'Gery',
+					name: 'Geryenko Hawsen',
+					phone: '070-1111-2222',
+					email: 'gery@gery.com',
+				},
+				{
+					id: 'Ozawa',
+					name: 'Ozawa Kun',
+					phone: '070-3333-4444',
+					email: 'ozawa@kun.com',
+				},
+			],
+		};
+	},
+};
+</script>
