1abf03a49e0d8458f171b70c4f70aadd57eb7088
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-16 20:03:21 +0900

    add friend-contact component

--- vue-cli-01-a-new-vue-project/src/App.vue
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -2,30 +2,33 @@
 	<section>
 		<h2>My Friends</h2>
 		<ul>
-			<li></li>
+			<friend-contact></friend-contact>
+			<friend-contact></friend-contact>
 		</ul>
 	</section>
 </template>
 
 <script>
+import FriendContact from './components/FriendContact.vue';
 export default {
-	data() {
-		return {
-			friends: [
-				{
-					id: 'Gery',
-					name: 'Geryenko Hawsen',
-					phone: '070-1111-2222',
-					email: 'gery@gery.com',
-				},
-				{
-					id: 'Ozawa',
-					name: 'Ozawa Kun',
-					phone: '070-3333-4444',
-					email: 'ozawa@kun.com',
-				},
-			],
-		};
-	},
+    data() {
+        return {
+            friends: [
+                {
+                    id: "Gery",
+                    name: "Geryenko Hawsen",
+                    phone: "070-1111-2222",
+                    email: "gery@gery.com",
+                },
+                {
+                    id: "Ozawa",
+                    name: "Ozawa Kun",
+                    phone: "070-3333-4444",
+                    email: "ozawa@kun.com",
+                },
+            ],
+        };
+    },
+    components: { FriendContact }
 };
 </script>
---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+	<li>
+		<h2>{{ friend.name }}</h2>
+		<button @click="toggleDetailsAreVisible">Show Details</button>
+		<ul v-if="detailsAreVisible">
+			<li><strong>Phone:</strong> {{ friend.phone }}</li>
+			<li><strong>Email:</strong> {{ friend.email }}</li>
+		</ul>
+	</li>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			detailsAreVisible: false,
+			friend: {
+				id: 'Gery',
+				name: 'Geryenko Hawsen',
+				phone: '070-1111-2222',
+				email: 'gery@gery.com',
+			},
+		};
+	},
+	methods: {
+		toggleDetailsAreVisible() {
+			this.detailsAreVisible = !this.detailsAreVisible;
+		},
+	},
+};
+</script>
