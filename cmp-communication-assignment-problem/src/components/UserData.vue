d333c8f998c93b5a25987bbe05fc1d8eabba4123
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-19 12:10:27 +0900

    update assignment 6

--- cmp-communication-assignment-problem/src/components/UserData.vue
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -1,30 +1,36 @@
 <template>
-	<form @submit.prevent="submitData">
-		<div>
-			<label>Name: </label>
-			<input type="text" v-model="enteredName" />
-		</div>
-		<div>
-			<label>Age: </label>
-			<input type="text" v-model="enteredAge" />
-		</div>
-		<button>Submit</button>
-	</form>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name: </label>
+      <input type="text" v-model="enteredName" placeholder="Your name" />
+    </div>
+    <div>
+      <label>Age: </label>
+      <input type="number" v-model="enteredAge" placeholder="Your age" />
+    </div>
+    <button>Submit</button>
+  </form>
 </template>
 
 <script>
 export default {
-	emits: ['add-contact'],
-	data() {
-		return {
-			enteredName: '',
-			enteredAge: '',
-		};
-	},
-	methods: {
-		submitData() {
-			this.$emit('add-contact', this.enteredName, this.enteredAge);
-		},
-	},
+  emits: ['add-contact'],
+  data() {
+    return {
+      enteredName: '',
+      enteredAge: '',
+    };
+  },
+  methods: {
+    submitData() {
+      if (this.enteredName === '' || this.enteredAge === '') {
+        alert('Please enter something!');
+      } else {
+        this.$emit('add-contact', this.enteredName, this.enteredAge);
+        this.enteredName = '';
+        this.enteredAge = '';
+      }
+    },
+  },
 };
 </script>
