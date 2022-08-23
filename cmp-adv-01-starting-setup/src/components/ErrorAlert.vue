37a5ca17bca92ee3833e9e7cc52558d44259e75a
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-23 21:27:12 +0900

    slot for dialog modal error alart

---
+++ cmp-adv-01-starting-setup/src/components/ErrorAlert.vue
@@ -0,0 +1,18 @@
+<template>
+    <dialog open>
+        <slot></slot>
+    </dialog>
+</template>
+
+<style scoped>
+dialog {
+    position: fixed;
+    margin: 0;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    background-color: white;
+    box-shadow: 0 2px 8px black;
+    padding: 1rem;
+}
+</style>
--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,6 +1,37 @@
 <template>
-    <h2>
-        <h2>Manage Goals</h2>
-        <input type="text">
-    </h2>
+  <h2>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is NG!</h2>
+      <p>enter something please...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </h2>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
