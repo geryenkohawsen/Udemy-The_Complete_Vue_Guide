4b590f091e371dc5c21f0a1d57c4c1cf62057858
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-30 10:00:28 +0900

    setup learning resources app

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official guide for Vue.js',
+          link: 'https://vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org/',
+        },
+      ],
+    };
+  },
+};
+</script>
