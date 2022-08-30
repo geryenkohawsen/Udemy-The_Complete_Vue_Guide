ee4dbd8b63e38af1a4ddd113202c7164b856d0cb
Geryenko Hawsen <gery.zarons@gmail.com>
2022-08-30 11:33:22 +0900

    add styling and StoredResources component

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,19 +1,13 @@
 <template>
-  <ul>
-    <learning-resource
-      v-for="res in storedResources"
-      :key="res.id"
-      v-bind="res"
-    ></learning-resource>
-  </ul>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
   components: {
-    LearningResource,
+    StoredResources,
   },
   data() {
     return {
@@ -35,3 +29,19 @@ export default {
   },
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ prj-cmp-01-starting-setup/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="res in resources"
+      :key="res.id"
+      v-bind="res"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+  props: ['resources'],
+  components: {
+    LearningResource,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
