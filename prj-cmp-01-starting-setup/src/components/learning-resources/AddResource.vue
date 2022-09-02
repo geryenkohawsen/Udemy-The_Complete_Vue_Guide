aa323cddfd3840eb5e9a26c0fdb006c08408ca70
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-02 22:05:17 +0900

    add modal dialog

--- prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+    <template #default>
+      <p>Unfortunately, at least one input is invalid...</p>
+      <p>Please check your input again!</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -26,17 +35,34 @@
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton.vue';
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
-
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
     },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
   },
+  components: { BaseButton },
 };
 </script>
 
