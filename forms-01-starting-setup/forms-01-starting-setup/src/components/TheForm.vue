fb4b0f7e100792558ae4a4b78cddb4d17f059241
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-05 02:44:34 +0900

    custom rating control component

--- forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
@@ -96,6 +96,9 @@
       </div>
     </div>
     <div class="form-control">
+      <rating-control></rating-control>
+    </div>
+    <div class="form-control">
       <input
         type="checkbox"
         name="confirm-terms"
@@ -111,7 +114,11 @@
 </template>
 
 <script>
+  import RatingControl from './RatingControl.vue';
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName: '',
