d67fa84403811ee03c51dc7f7da09c5f223f79ba
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-04 23:33:46 +0900

    v-model and v-model modifiers

--- forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,12 +1,12 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div class="form-control">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input id="user-name" name="user-name" type="text" v-model="userName" />
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="userAge" ref="ageInput"/>
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
@@ -51,6 +51,26 @@
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log('userName: ' + this.userName);
+      this.userName = '';
+      console.log(this.userAge)
+      console.log(this.$refs.ageInput.value)
+      console.log(this.age)
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
