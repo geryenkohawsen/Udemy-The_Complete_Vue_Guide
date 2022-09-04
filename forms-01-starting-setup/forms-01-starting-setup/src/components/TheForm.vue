87f7ee9bd155c34e5a57d317171f2f8fb5fc24b5
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-05 00:30:15 +0900

    userName validation using @blue

--- forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
+++ forms-01-starting-setup/forms-01-starting-setup/src/components/TheForm.vue
@@ -1,8 +1,15 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="userName" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model.trim="userName"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name.</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -113,6 +120,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: 'pending',
     };
   },
   methods: {
@@ -139,6 +147,13 @@ export default {
       console.log(this.confirm);
       this.confirm = false;
     },
+    validateInput() {
+      if (this.userName === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
+    },
   },
 };
 </script>
@@ -155,6 +170,13 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
