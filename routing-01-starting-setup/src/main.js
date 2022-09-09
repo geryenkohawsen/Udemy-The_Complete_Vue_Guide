a554d43effa85ccd84a5c704569603d8f40a557d
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-09 21:50:36 +0900

    register routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,12 +2,19 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/teams/UsersList.vue';
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [],
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/teams', component: UsersList },
+  ],
 });
 
 const app = createApp(App);
+
+app.use(router);
 
 app.mount('#app');
