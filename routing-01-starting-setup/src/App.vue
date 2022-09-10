110a25f9f7976993fe5f9f4b0ee0a1bbdfb88dc7
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-10 21:18:37 +0900

    router link

--- routing-01-starting-setup/src/App.vue
+++ routing-01-starting-setup/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation></the-navigation>
   <main><router-view></router-view></main>
 </template>
 
@@ -12,7 +12,6 @@ export default {
   },
   data() {
     return {
-      activePage: 'teams-list',
       teams: [
         { id: 't1', name: 'Frontend Engineers', members: ['u1', 'u2'] },
         { id: 't2', name: 'Backend Engineers', members: ['u1', 'u2', 'u3'] },
@@ -32,11 +31,6 @@ export default {
       teams: this.teams,
       users: this.users,
     };
-  },
-  methods: {
-    setActivePage(page) {
-      this.activePage = page;
-    },
   },
 };
 </script>
