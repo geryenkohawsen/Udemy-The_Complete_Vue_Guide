ce7a0f5ec190b64ae1fb853ae68d9fd093d79323
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-10 21:30:42 +0900

    programmatic navigation

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,6 +1,12 @@
 <template>
   <ul>
-    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+    <button @click="confirmInput">Change Page</button>
+    <user-item
+      v-for="user in users"
+      :key="user.id"
+      :name="user.fullName"
+      :role="user.role"
+    ></user-item>
   </ul>
 </template>
 
@@ -12,6 +18,11 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      this.$router.push('/teams');
+    },
+  },
 };
 </script>
 
