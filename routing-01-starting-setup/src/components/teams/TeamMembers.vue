3591ea8f4492be675568ff79ad291c4581985c7f
Geryenko Hawsen <gery.zarons@gmail.com>
2022-09-11 12:26:15 +0900

    pass router params as props

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -18,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem,
   },
@@ -28,9 +29,9 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route) {
+    loadTeamMembers(teamId) {
       // we can use console.log(this.$route) to see all available route in the project
-      const teamId = route.params.teamId; // holds all the route parameters used in the current page
+      // const teamId = route.params.teamId; // holds all the route parameters used in the current page
       const selectedTeam = this.teams.find((team) => team.id === teamId);
       const members = selectedTeam.members;
       const selectedMembers = [];
